21509351df241b78a013114fc378f876f7dfddcd
UNT 1 <dev25UNT>
2022-11-03 11:39:50 +0530

    ui improved

--- src/components/EmailSection/index.jsx
+++ src/components/EmailSection/index.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./emailSection.css";
 import chiron from "../../assets/icons/chironRight.svg";
 import e1 from "../../assets/icons/email1.svg";
 
 const EmailSection = () => {
+  const [eId, setId] = useState(1);
+
+  const emailClicked = (e) => {
+    e.preventDefault();
+    setId(e.target.id);
+  };
   return (
     <div className="mainEmail">
       <div className="emailHead">
@@ -12,9 +18,18 @@ const EmailSection = () => {
           More <img src={chiron} />
         </span>
       </div>
-      {[1, 2, 3].map(() => {
+      {[1, 2, 3].map((item, index) => {
+        console.log(index);
+        console.log(eId, "eId");
         return (
-          <div className="emailInfo">
+          <div
+            className={
+              eId == index ? "emailInfo emailInfoClicked" : "emailInfo"
+            }
+            id={index}
+            key={item}
+            onClick={(e) => emailClicked(e)}
+          >
             <div className="emailPerson">
               <img src={e1} />
               <h3 className="eName">Elizabeth</h3>
